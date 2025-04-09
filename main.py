660d83f4b7ca1fd8ce914f97bc547eb60deeb970
Fred Morrison <frederick.morrison@outlook.com>
2025-04-09 12:47:52 -0600

    store totals in budget dictionary

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# 1. establish budget
+budget = {'income': {}, 'expenses': {}, 'totals': {'income': 0, 'expenses': 0}}
+
 def get_dollar_amount():
     while True:
         try:
@@ -20,7 +23,13 @@ def get_type():
 
 
 def get_category():
-    key = input('Enter the category')
+    while True:
+        key = input('Enter the category: ')
+        if key is None or len(key) == 0:
+            print('Please enter a category')
+        else:
+            break
+
     return key.lower()
 
 
@@ -33,39 +42,12 @@ def ask_yes_no_question(question):
     return False
 
 
-#### MAIN PROGRAM STARTS HERE ####
-# 1. establish budget
-budget = {'income': {}, 'expenses': {}}
-total_income = 0
-total_expenses = 0
+def update_budget(top_key, cat_key, money,):
+    if cat_key not in budget[top_key].keys():
+        budget[top_key][cat_key] = 0
+    budget[top_key][cat_key] += money
 
-while True:
-    # 2. get main user input
-    keep_going = ask_yes_no_question("Do you want to enter more budget entries? ")
-    if not keep_going:
-        break
-
-    type_key = get_type()
-    print(type_key)
-
-    category_key = get_category()
-
-    print('Enter dollar amount')
-    dollar_amount = get_dollar_amount()
-    print(dollar_amount)
-
-    print(type_key, category_key, dollar_amount)
-    budget[type_key][category_key] = dollar_amount
-
-    if category_key == 'income':
-        total_income += dollar_amount
-    else:
-        total_expenses += dollar_amount
-
-    print('Budget so far: ' + str(budget))
-
-print('final budget: ' + str(budget))
-print(total_income, total_expenses)
+    budget['totals'][top_key] += money
 
 
 def display_budget():
@@ -75,5 +57,19 @@ def display_budget():
             amount = float(dollar_string)
             print(' ' * 2 + cat + ' ' + str(amount))
 
+
+#### MAIN PROGRAM LOGIC STARTS HERE ####
+
+while True:
+    # 2. get main user input
+    keep_going = ask_yes_no_question("Do you want to enter more budget entries? ")
+    if not keep_going:
+        break
+
+    type_key = get_type()
+    category_key = get_category()
+    dollar_amount = get_dollar_amount()
+
+    update_budget(type_key, category_key, dollar_amount)
 
 display_budget()
